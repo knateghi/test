d37bb45c12dfc3c79745ec59b5594634498899e1
Kobra Nateghi <knateghi@my.centennialcollege.ca>
2019-06-21 16:00:40 -0400

    repository initialized

---
+++ Test2/Controllers/HelloWorldController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Test2.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //GET: /HelloWorld
+        public string Index()
+        {
+            return ("This is my default action...");
+        }
+
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello" + name;
+            ViewData["NumTimes"] = numTimes;
+            return View();
+        }
+    }
+
+}
---
+++ Test2/Data/Test2Context.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Test2.Models
+{
+    public class Test2Context : DbContext
+    {
+        public Test2Context (DbContextOptions<Test2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Test2.Models.Movies> Movies { get; set; }
+    }
+}
